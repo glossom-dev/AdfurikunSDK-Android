7744f979c723b1067740095bd81dfc6b1868a0e1
Sungjin Kan <z.sungjin.kan@gree.net>
2020-03-05 17:46:15 +0900

    first commit [ 3.0.0 ~ 3.1.2 ]

---
+++ tools/installAAR.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+GROUP=jp.adfully
+ARTIFACT=adfurikunsdk
+VERSION=3.1.2
+PACKAGING=aar
+
+mvn clean dependency:copy -DgroupId=$GROUP -DartifactId=$ARTIFACT -Dversion=$VERSION -Dtype=$PACKAGING -U
+
+mvn install:install-file -DgroupId=$GROUP -DartifactId=$ARTIFACT -Dversion=$VERSION \
+-Dfile=target/$ARTIFACT-$VERSION.$PACKAGING \
+-Dpackaging=$PACKAGING \
+-DpomFile=target/$ARTIFACT-$VERSION.pom \
+-DlocalRepositoryPath=../ \
+-DcreateChecksum=true
+
+md5 target/$ARTIFACT-$VERSION.$PACKAGING
+rm -r ./target
+
