7e0a8c023f8c01ee6de34ee6ab3f2d142f3d9ed5
Youngdoo Lee <z.youngdoo.lee@gree.net>
2020-11-04 12:05:20 +0900

    update sdk 3.6.1

--- tools/installAAR.sh
+++ tools/installAAR.sh
@@ -2,7 +2,7 @@
 
 GROUP=jp.adfully
 ARTIFACT=adfurikunsdk
-VERSION=3.6.0
+VERSION=3.6.1
 PACKAGING=aar
 
 mvn clean dependency:copy -DgroupId=$GROUP -DartifactId=$ARTIFACT -Dversion=$VERSION -Dtype=$PACKAGING -U
